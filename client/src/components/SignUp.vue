27df0d7543de18b38645f8ccb62ab3d2379a3f06
Mustafa Muftah <mustafa.mufta7@gmail.com>
2025-08-02 22:03:31 +0300

    add tailwind

---
+++ client/src/components/SignUp.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+ <div class="flex flex-row  min-h-screen">
+    <div class="flex items-center basis-1/3 bg-1 h-screen hover:basis-2/3 transition-all duration-500 justify-center">
+       <div class="flex justify-center"> Heloo World </div>
+    </div>
+     <div class="basis-2/3 h-screen">
+        Heloo World
+    </div>
+ </div>
+</template>
+
+<style scoped>
+</style>
